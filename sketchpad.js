78940250c60f473fd4f8f3b9b453bb4de9201917
Axl M <axl.mccracken@gmail.com>
2015-03-12 00:42:07 -0500

    add options pane and prepare container div for divGrid

---
+++ sketchpad.js
@@ -0,0 +1,13 @@
+// Create a 16x16 grid of square divs inside the container div
+// Set up a hover effect so it changes the color of the square when mouseenter, leaving a pixelated trail through the divgrid
+// Add a button to the top of the screen which will clear the grid
+    // This should also give a dialog box allowing the user how many squares to user per side of grid
+    // Once entered, new grid should be generated in same total space as before (such as 960px)
+
+$(document).ready(function() {
+    // Code for the pull-tab. Will go elsewhere eventually.
+    $('.pull-tab').on("click",function() {
+        $('.buttons-panel').slideToggle();
+    });
+    // Draw the initial grid of 16x16 squares
+});
